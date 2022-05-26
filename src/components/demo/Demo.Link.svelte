59aca5fde5477ecfa5d6578fa37f8ad857b1cebe
Russell Goldenberg <russellgoldenberg@gmail.com>
2022-05-26 13:20:54 -0400

    Initial commit

---
+++ src/components/demo/Demo.Link.svelte
@@ -0,0 +1,4 @@
+<section id="demo-link">
+  <h2>Link</h2>
+  <a href="elements">See default element styles page</a>
+</section>
---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { setContext } from "svelte";
+  import { browser } from "$app/env";
+  import Meta from "$components/Meta.svelte";
+  import App from "$components/App.svelte";
+  import copy from "$data/doc.json";
+  import version from "$utils/version.js";
+
+  version();
+
+  const preloadFont = [
+    "https://pudding.cool/assets/fonts/tiempos/TiemposTextWeb-Regular.woff2",
+    "https://pudding.cool/assets/fonts/tiempos/TiemposTextWeb-Bold.woff2",
+    "https://pudding.cool/assets/fonts/national/National2Web-Regular.woff2",
+    "https://pudding.cool/assets/fonts/national/National2Web-Bold.woff2"
+  ];
+
+  const { title, description, url, keywords } = copy;
+  setContext("copy", copy);
+</script>
+
+<Meta {title} {description} {url} {preloadFont} {keywords} />
+<App />
