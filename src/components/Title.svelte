1b430f0d7bcc721cc28be890796e7a02015399c3
russellgoldenberg <russellgoldenberg@gmail.com>
2022-06-16 15:52:50 -0400

    things

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -33,6 +33,35 @@
     font-size: var(--16px);
   }
 
+  :global(h1 mark) {
+    position: relative;
+    display: inline-block;
+  }
+
+  :global(h1 mark:before) {
+    left: 0;
+    background: url("assets/images/title-before.jpg");
+    transform: translate(-100%, 0);
+  }
+
+  :global(h1 mark:after) {
+    right: 0;
+    background: url("assets/images/title-after.jpg");
+    transform: translate(100%, 0);
+  }
+
+  :global(h1 mark:before, h1 mark:after) {
+    content: "";
+    position: absolute;
+    top: 0;
+    width: 12rem;
+    height: 100%;
+    background-position: 50% 50%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    border: 4px solid var(--color-primary);
+  }
+
   @media only screen and (min-width: 30rem) {
     h1 {
       font-size: var(--64px);
