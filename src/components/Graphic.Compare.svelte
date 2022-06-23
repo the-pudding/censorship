7337e1882c13a25c70a67df3fd556b66c54ed040
russellgoldenberg <russellgoldenberg@gmail.com>
2022-06-23 08:37:43 -0400

    add images

--- src/components/Graphic.Compare.svelte
+++ src/components/Graphic.Compare.svelte
@@ -1,3 +1,31 @@
-<figure>
-  <h2>Compare Chart</h2>
-</figure>
+<script>
+  export let clips;
+</script>
+
+<div>
+  {#each clips as { image, alt, figcaption }}
+    <figure>
+      <img src="assets/images/{image}.jpg" {alt} />
+      <figcaption>{figcaption}</figcaption>
+    </figure>
+  {/each}
+</div>
+
+<style>
+  div {
+    display: flex;
+    flex-direction: column;
+    max-width: 1280px;
+    margin: 32px auto;
+  }
+
+  figcaption {
+    text-align: center;
+  }
+
+  @media screen and (min-width: 40rem) {
+    div {
+      flex-direction: row;
+    }
+  }
+</style>
