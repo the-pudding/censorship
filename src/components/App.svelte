3ff0bcf579ab81136a54306170da327dc8187254
russellgoldenberg <russellgoldenberg@gmail.com>
2022-06-30 10:14:11 -0400

    tabs

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,47 +1,47 @@
 <script>
-  import { getContext } from "svelte";
-  import Title from "$components/Title.svelte";
-  import Footer from "$components/Footer.svelte";
-  import Toggle from "$components/Graphic.Toggle.svelte";
-  import Quote from "$components/Graphic.Quote.svelte";
-  import Compare from "$components/Graphic.Compare.svelte";
-  import Scroll from "$components/Graphic.Scroll.svelte";
-  import Video from "$components/Graphic.Video.svelte";
-  // import Toggler from "$components/helpers/Toggle.svelte";
-  import MotionToggle from "$lib/components/helpers/MotionToggle.svelte";
+	import { getContext } from "svelte";
+	import Title from "$components/Title.svelte";
+	import Footer from "$components/Footer.svelte";
+	import Toggle from "$components/Graphic.Toggle.svelte";
+	import Quote from "$components/Graphic.Quote.svelte";
+	import Compare from "$components/Graphic.Compare.svelte";
+	import Scroll from "$components/Graphic.Scroll.svelte";
+	import Video from "$components/Graphic.Video.svelte";
+	// import Toggler from "$components/helpers/Toggle.svelte";
+	import MotionToggle from "$lib/components/helpers/MotionToggle.svelte";
 
-  const copy = getContext("copy");
-  const data = getContext("data");
-  const components = { Toggle, Quote, Compare, Scroll, Video };
+	const copy = getContext("copy");
+	const data = getContext("data");
+	const components = { Toggle, Quote, Compare, Scroll, Video };
 </script>
 
 <article>
-  <div class="motion">
-    <!-- <Toggler label="Motion" /> -->
-    <MotionToggle />
-  </div>
-  <Title />
-  {#each copy.sections as { id, chunks }}
-    <section {id}>
-      {#each chunks as { type, text, component, clips, steps, classname }}
-        <svelte:element this={type} class={classname}>
-          {#if text}
-            {@html text}
-          {:else}
-            <svelte:component this={components[component]} {clips} {steps} />
-          {/if}
-        </svelte:element>
-      {/each}
-    </section>
-  {/each}
+	<div class="motion">
+		<!-- <Toggler label="Motion" /> -->
+		<MotionToggle />
+	</div>
+	<Title />
+	{#each copy.sections as { id, chunks }}
+		<section {id}>
+			{#each chunks as { type, text, component, clips, steps, classname }}
+				<svelte:element this={type} class={classname}>
+					{#if text}
+						{@html text}
+					{:else}
+						<svelte:component this={components[component]} {clips} {steps} />
+					{/if}
+				</svelte:element>
+			{/each}
+		</section>
+	{/each}
 </article>
 
 <Footer />
 
 <style>
-  .motion {
-    position: fixed;
-    bottom: 16px;
-    right: 16px;
-  }
+	.motion {
+		position: fixed;
+		bottom: 16px;
+		right: 16px;
+	}
 </style>
--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -1,100 +1,100 @@
 <script>
-  import { getContext } from "svelte";
-  import { base } from "$app/paths";
-  const copy = getContext("copy");
+	import { getContext } from "svelte";
+	import { base } from "$app/paths";
+	const copy = getContext("copy");
 </script>
 
 <section id="hero">
-  <h1
-    style="--pathBefore: url({base}/assets/images/title-before.jpg); --pathAfter: url({base}/assets/images/title-after.jpg);"
-  >
-    {@html copy.hed}
-  </h1>
-  <time>{copy.time}</time>
-  <p>{@html copy.byline1}</p>
-  <p>{@html copy.byline2}</p>
+	<h1
+		style="--pathBefore: url({base}/assets/images/title-before.jpg); --pathAfter: url({base}/assets/images/title-after.jpg);"
+	>
+		{@html copy.hed}
+	</h1>
+	<time>{copy.time}</time>
+	<p>{@html copy.byline1}</p>
+	<p>{@html copy.byline2}</p>
 </section>
 
 <style>
-  section {
-    max-width: var(--col-width);
-    margin: var(--32px) auto;
-    text-align: center;
-  }
+	section {
+		max-width: var(--col-width);
+		margin: var(--32px) auto;
+		text-align: center;
+	}
 
-  h1 {
-    font-weight: bold;
-    text-transform: uppercase;
-    font-size: var(--40px);
-    line-height: 1.2;
-  }
+	h1 {
+		font-weight: bold;
+		text-transform: uppercase;
+		font-size: var(--40px);
+		line-height: 1.2;
+	}
 
-  p {
-    margin: 4px auto;
-  }
+	p {
+		margin: 4px auto;
+	}
 
-  p:last-of-type,
-  time {
-    font-size: var(--16px);
-  }
+	p:last-of-type,
+	time {
+		font-size: var(--16px);
+	}
 
-  :global(h1 mark) {
-    position: relative;
-    display: inline-block;
-  }
+	:global(h1 mark) {
+		position: relative;
+		display: inline-block;
+	}
 
-  :global(h1 mark:before, h1 mark:after) {
-    display: none;
-  }
+	:global(h1 mark:before, h1 mark:after) {
+		display: none;
+	}
 
-  :global(h1 mark:before) {
-    left: 0;
-    background: var(--pathBefore);
-    transform: translate(-100%, 0);
-  }
+	:global(h1 mark:before) {
+		left: 0;
+		background: var(--pathBefore);
+		transform: translate(-100%, 0);
+	}
 
-  :global(h1 mark:after) {
-    right: 0;
-    background: var(--pathAfter);
-    transform: translate(100%, 0);
-  }
+	:global(h1 mark:after) {
+		right: 0;
+		background: var(--pathAfter);
+		transform: translate(100%, 0);
+	}
 
-  :global(h1 mark:before, h1 mark:after) {
-    content: "";
-    position: absolute;
-    top: 0;
-    width: 8rem;
-    height: 100%;
-    background-position: 50% 50%;
-    background-repeat: no-repeat;
-    background-size: cover;
-    border: 4px solid var(--color-primary);
-  }
+	:global(h1 mark:before, h1 mark:after) {
+		content: "";
+		position: absolute;
+		top: 0;
+		width: 8rem;
+		height: 100%;
+		background-position: 50% 50%;
+		background-repeat: no-repeat;
+		background-size: cover;
+		border: 4px solid var(--color-primary);
+	}
 
-  @media only screen and (min-width: 30rem) {
-    h1 {
-      font-size: var(--64px);
-    }
+	@media only screen and (min-width: 30rem) {
+		h1 {
+			font-size: var(--64px);
+		}
 
-    p:last-of-type,
-    time {
-      font-size: var(--18px);
-    }
-  }
+		p:last-of-type,
+		time {
+			font-size: var(--18px);
+		}
+	}
 
-  @media only screen and (min-width: 60rem) {
-    h1 {
-      font-size: var(--96px);
-    }
+	@media only screen and (min-width: 60rem) {
+		h1 {
+			font-size: var(--96px);
+		}
 
-    p:last-of-type,
-    time {
-      font-size: var(--20px);
-    }
+		p:last-of-type,
+		time {
+			font-size: var(--20px);
+		}
 
-    :global(h1 mark:before, h1 mark:after) {
-      width: 11rem;
-      display: block;
-    }
-  }
+		:global(h1 mark:before, h1 mark:after) {
+			width: 11rem;
+			display: block;
+		}
+	}
 </style>
